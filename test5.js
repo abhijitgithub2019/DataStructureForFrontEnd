bb4a7439d5bc881dfdf407d7d8714fd68f887146
abhijit <abhijit.patra224@gmail.com>
2025-07-28 13:51:27 +0530

    curry

--- test5.js
+++ test5.js
@@ -379,45 +379,60 @@
 // console.log(values);
 
 
-var orangesRotting = function(grid) {
-    const height = grid.length;
-    const width = grid[0].length;
-    let fresh = 0;
-    const queue = [];
-    for (let i = 0; i < height; i++) {
-      for (let j = 0; j < width; j++) {
-        if (grid[i][j] === 2) queue.push([i, j]);
-        if (grid[i][j] === 1) fresh++;
-      }
-    }
-    let minute = 0;
-    while (queue.length) {
-      const size = queue.length;
-      for (let i = 0; i < size; i++) {
-        const [x, y] = queue.shift();
-        if (x - 1 >= 0 && grid[x - 1][y] === 1) {
-          grid[x - 1][y] = 2;
-          fresh--;
-          queue.push([x - 1, y]);
-        }
-        if (x + 1 < height && grid[x + 1][y] === 1) {
-          grid[x + 1][y] = 2;
-          fresh--;
-          queue.push([x + 1, y]);
-        }
-        if (y - 1 >= 0 && grid[x][y - 1] === 1) {
-          grid[x][y - 1] = 2;
-          fresh--;
-          queue.push([x, y - 1]);
-        }
-        if (y + 1 < width && grid[x][y + 1] === 1) {
-          grid[x][y + 1] = 2;
-          fresh--;
-          queue.push([x, y + 1]);
-        }
-      }
-      if (queue.length > 0) minute++;
-    }
-    return fresh === 0 ? minute : -1;
-  };
+// var orangesRotting = function(grid) {
+//     const height = grid.length;
+//     const width = grid[0].length;
+//     let fresh = 0;
+//     const queue = [];
+//     for (let i = 0; i < height; i++) {
+//       for (let j = 0; j < width; j++) {
+//         if (grid[i][j] === 2) queue.push([i, j]);
+//         if (grid[i][j] === 1) fresh++;
+//       }
+//     }
+//     let minute = 0;
+//     while (queue.length) {
+//       const size = queue.length;
+//       for (let i = 0; i < size; i++) {
+//         const [x, y] = queue.shift();
+//         if (x - 1 >= 0 && grid[x - 1][y] === 1) {
+//           grid[x - 1][y] = 2;
+//           fresh--;
+//           queue.push([x - 1, y]);
+//         }
+//         if (x + 1 < height && grid[x + 1][y] === 1) {
+//           grid[x + 1][y] = 2;
+//           fresh--;
+//           queue.push([x + 1, y]);
+//         }
+//         if (y - 1 >= 0 && grid[x][y - 1] === 1) {
+//           grid[x][y - 1] = 2;
+//           fresh--;
+//           queue.push([x, y - 1]);
+//         }
+//         if (y + 1 < width && grid[x][y + 1] === 1) {
+//           grid[x][y + 1] = 2;
+//           fresh--;
+//           queue.push([x, y + 1]);
+//         }
+//       }
+//       if (queue.length > 0) minute++;
+//     }
+//     return fresh === 0 ? minute : -1;
+//   };
   
+
+function curry(func) {
+
+    return function curried(...args) {
+      if (args.length >= func.length) {
+        return func.apply(this, args);
+      } else {
+        return function(...args2) {
+          return curried.apply(this, args.concat(args2));
+        }
+      }
+    };
+  
+  }
+//   console.log(mul(2)(3)(7));  
